13efcf86da1527152b20dcdce84177d2423b85e5
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-02-18 09:39:28 +0500

    Return token from context

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const modules = require('./src/modules/')
 const CONFIG = require('./src/config/config.js')
 const { ApolloServer } = require('apollo-server-express')
 
-const server = new ApolloServer({ modules, context: (con) => {
-	console.log(con)
-}})
+const server = new ApolloServer({ 
+	modules,
+	context: ({ req }) => ({token: req.headers.token}) 
+})
 
 const app = express()
 server.applyMiddleware({ app, path: '/graphql' })
 const httpServer = http.createServer(app)
 server.installSubscriptionHandlers(httpServer)
 
-httpServer.listen({ port: CONFIG.PORT }, () => console.log(`Ready at htpp://localhost:${CONFIG.PORT}`))
+httpServer.listen({ port: CONFIG.PORT }, () => console.log(`Ready at http://localhost:${CONFIG.PORT}`))
